54c9b78ecda7fbfddea1cd23f8bb3d3ec917528d
Hassan Habib Tahir <47211929+HassanHabibTahir@users.noreply.github.com>
2023-09-12 16:30:53 +0500

    fixing by hassan habib tahir

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,52 @@
+import { Router } from "express";
+import { catchAsync, upload } from "../middleware/index.js";
+import {
+  register,
+  login,
+  forgetPassword,
+  verificationOTP,
+  verificationOTPForSignup,
+  changePassword,
+  getUser,
+  logout,
+  changeAppPassword,
+} from "../controller/auth.controller.js";
+import passport from "passport";
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+export const authRoutes = Router();
+
+// Register
+authRoutes.post("/register", catchAsync(register));
+
+// Login
+authRoutes.post("/login", catchAsync(login));
+
+// forgot-password
+authRoutes.post("/forgot-password", catchAsync(forgetPassword));
+
+// verification otp
+authRoutes.post("/otpVerification", catchAsync(verificationOTP));
+
+// verification otp for signup
+authRoutes.post(
+  "/otpVerification-signup",
+  catchAsync(verificationOTPForSignup)
+);
+
+// change-password
+
+authRoutes.post("/change-password", catchAsync(changePassword));
+authRoutes.post("/changeAppPassword", catchAsync(changeAppPassword));
+
+// get-user
+authRoutes.get(
+  "/getUser",
+  passport.authenticate("jwt", { session: false }),
+  catchAsync(getUser)
+);
+
+// logout
+authRoutes.post("/logout", catchAsync(logout));
